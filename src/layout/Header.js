c3f4d49d10accbda0a1a1f6ffd79f5ddfb65a79b
Martin Bruno <mb.martinbruno@gmail.com>
2021-09-13 19:03:54 +0200

    change header font-size

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -5,24 +5,28 @@ import "animate.css";
 
 const Header = () => {
   return (
-    <header className="header">
-      <h2 className="header__title">
-        Hi! My name is <span>Martin Bruno</span>
-      </h2>
-      <h3 className="header__subtitle">
-        I'm a {""}
-        <span>
-          <Typical
-            steps={["Lawyer and...", 3000, "Frontend Developer!", 4000]}
-            loop={Infinity}
-            wrapper="b"
-          />
-        </span>
-      </h3>
-      <a href="#about">
-        <FiArrowDownCircle className="header__icon animate__animated animate__bounce animate__infinite" />
-      </a>
-    </header>
+    <>
+      <header className="header">
+        <h2 className="header__title">
+          Hi! My name is <span>Martin Bruno</span>
+        </h2>
+        <h3 className="header__subtitle">
+          I'm a {""}
+          <span>
+            <Typical
+              steps={["Lawyer and...", 3000, "Frontend Developer!", 4000]}
+              loop={Infinity}
+              wrapper="b"
+            />
+          </span>
+        </h3>
+        <div className="header__icon__container">
+          <a href="#about">
+            <FiArrowDownCircle className="header__icon animate__animated animate__bounce animate__infinite" />
+          </a>
+        </div>
+      </header>
+    </>
   );
 };
 
